2042a2e29a149f9f71f0835278446429d6a28322
Marc LaBelle <mjlabe@gmail.com>
2025-03-15 12:23:36 -0400

    Create buffalo_theater_aggregator.py

---
+++ buffalo_theater_aggregator.py
@@ -0,0 +1,17 @@
+from pandas import Dataframe
+
+from ds9 import Scraper
+
+
+def format_sites_data(scraped_data):
+    pass
+    return data
+    
+    
+def main()
+    scraper = Scraper(whatever_ai_settings=settings.tge_settings).
+    scraped_sites_data = scraper.aggregate(
+        sites=settings.sites,
+    )
+    df = Dataframe(formatted_sites_data(scraped_sites_data)
+    st.show(df)
