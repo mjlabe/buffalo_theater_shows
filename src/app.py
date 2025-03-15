1376afdb7e32413aad6a064b02e52a9664a3e7bc
Marc LaBelle <mjlabe@gmail.com>
2025-03-14 22:57:29 -0400

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,3 +1,19 @@
 import streamlit as st
 
-st.write("Hello world")
+import pandas as pd
+
+from scrapers import theater_scraper
+
+from settings import theaters
+
+
+for theater in theaters:
+    shows = theater_scraper(theater.get("shows_url"))
+    theater_names.append(theater.get("name"))
+    show_data = pd.DataFrame({
+        "theater": theater_names,
+        "shows": shows
+    })
+    
+st.write("Here's our first attempt at using data to create a table:")
+st.write(show_data)
